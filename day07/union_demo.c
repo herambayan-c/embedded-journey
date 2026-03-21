0d0f5dcdf6e73f9286cede3f3e7264c0ccc244d4
Herambayan C <herambayanc31@gmail.com>
2026-03-21 19:50:46 +0530

    Day 08 - Unions and Endianness

---
+++ day07/union_demo.c
@@ -0,0 +1,19 @@
+// Define a union called RawData with:
+//   - unsigned int raw (4 bytes)
+//   - unsigned char bytes[4]
+// Store a value in raw: 0xDEADBEEF
+// Print raw as hex
+// Print each byte separately using bytes[]
+// Add a comment explaining what you observe about byte order
+#include<stdio.h>
+int main(){
+    union RawData
+    {
+        unsigned int raw;
+        unsigned char bytes[4];
+    };
+    union RawData a = {0XDEADBEEF};
+    printf("The raw as hex %x \n",a.raw);
+    printf("Printing byte by byte %x %x %x %x ",a.bytes[0],a.bytes[1],a.bytes[2],a.bytes[3]);
+    
+}
